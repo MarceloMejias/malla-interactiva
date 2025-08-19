6e29b671438e009ff41f84358a681d0b51880a85
Marcelo Mejías <65011000+MarceloMejias@users.noreply.github.com>
2025-08-19 13:03:44 -0400

    Revert "route.ts fix"
    This reverts commit 12a2ec789f011649a57c16ccae9f9f5d98a156ae.

--- src/app/api/careers/[campus]/[code]/colors/route.ts
+++ src/app/api/careers/[campus]/[code]/colors/route.ts
@@ -2,8 +2,11 @@ import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function GET(req: any, { params }: any) {
+interface Params {
+  params: { campus: string; code: string };
+}
+
+export async function GET(req: Request, { params }: Params) {
   const { campus, code } = params;
   const filePath = path.join(process.cwd(), 'src', 'data', campus, `colors_${code}.json`);
   try {
